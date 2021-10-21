64a50c7682343eca0c832916863f7ccc315c6d32
jaison_js@hotmail.com <brsantos@@001>
2021-10-21 22:39:55 +0200

    Initial commit

---
+++ src/main/java/jado/springframework/sfdi/SfDiApplication.java
@@ -0,0 +1,13 @@
+package jado.springframework.sfdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfDiApplication.class, args);
+    }
+
+}
