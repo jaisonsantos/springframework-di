0fb1e0684b250f485f8686a7c366e0e507fea8dd
jaison_js@hotmail.com <brsantos@@001>
2021-10-23 16:50:30 +0200

    adding example of primary bean

--- src/main/java/jado/springframework/sfdi/controllers/MyController.java
+++ src/main/java/jado/springframework/sfdi/controllers/MyController.java
@@ -1,12 +1,18 @@
 package jado.springframework.sfdi.controllers;
 
+import jado.springframework.sfdi.services.PrimaryGreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final PrimaryGreetingService primaryGreetingService;
+
+    public MyController(PrimaryGreetingService primaryGreetingService) {
+        this.primaryGreetingService = primaryGreetingService;
+    }
+    
     public String sayHello() {
-        System.out.println("Hello World!!!");
-        return "Hi folks";
+        return primaryGreetingService.sayGreeting();
     }
 }
